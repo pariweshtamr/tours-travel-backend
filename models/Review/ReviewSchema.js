01a46e2500d2e6cc3c6afae19650661fc0c9a3dc
pariwesh <tamrpariwesh@gmail.com>
2023-02-11 21:07:40 +1100

    initial commit, added schemas

---
+++ models/Review/ReviewSchema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+const reviewSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Types.ObjectId,
+      ref: Tour,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reviewText: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+      deafult: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model("Review", reviewSchema)
---
+++ models/User/UserSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: 1,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      Type: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model("User", userSchema)
